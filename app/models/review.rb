191b50c80fd2649fadefb3012c0e60e57070996f
Student <example@example.com>
2020-01-17 13:25:15 -0800

    add red green testing for numericality and range of Rating on Reviews class

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,3 +1,15 @@
 class Review < ApplicationRecord
   belongs_to :product
+  validates :author, presence: true
+  validates :rating, presence: true, :numericality => {only_integer: true, greater_than_or_equal_to: 1, less_than_or_equal_to: 5}
+  validates :content_body, presence: true, length: {
+    minimum: 50,
+    maximum: 250,
+    too_short: "This review must have at least 50 characters",
+    too_long: "This review must have no more than 250 characters"
+  }
 end
+
+# numericality: { greater_than_or_equal_to: 1
+#
+# }

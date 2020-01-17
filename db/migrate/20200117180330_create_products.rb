8c82f3215d86a24f2775df39d8e1ee6ba0b4a018
Student <example@example.com>
2020-01-17 10:24:25 -0800

    add classes and database

--- db/migrate/20200117180330_create_products.rb
+++ db/migrate/20200117180330_create_products.rb
@@ -1,6 +1,9 @@
 class CreateProducts < ActiveRecord::Migration[5.2]
   def change
     create_table :products do |t|
+      t.string :prod_name
+      t.integer :cost
+      t.string :country_of_origin
     end
   end
 end

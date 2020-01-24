bf6661ff7ac4d8d853c284dd2a81a45391095b11
cidsinga <56556816+cidsinga@users.noreply.github.com>
2020-01-24 11:33:44 -0800

    add fix to bcrypt issue, not sure how it was fixed, but working now, possibly a server restart

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -15,7 +15,7 @@ before_action :set_user, only: [:show, :edit, :update, :destroy]
     @user = User.new(user_params)
     if @user.save
       session[:user_id] = @user.id
-      binding.pry
+      # binding.pry
       if current_user.admin === true
         flash[:notice] = "Welcome, Admin"
       else
@@ -42,6 +42,6 @@ end
 end
 
   def user_params
-    params.require(:user).permit(:email, :password, :password_confirmation, :admin)
+    params.require(:user).permit(:user_name, :email, :password, :password_confirmation, :admin)
   end
 end

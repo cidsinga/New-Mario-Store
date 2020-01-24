d5be1dc52e34744134a5e1ceba8fc6ca70efee02
cidsinga <56556816+cidsinga@users.noreply.github.com>
2020-01-24 09:12:16 -0800

    add addition of cost and C_O_O to prodect validation spec, tests pass

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -2,7 +2,9 @@ require 'rails_helper'
 
 describe Product do
   it { should have_many (:reviews) }
-  it { should validate_presence_of :prod_name, :cost, :country_of_origin }
+  it { should validate_presence_of :prod_name}
+  it { should validate_presence_of :cost}
+  it { should validate_presence_of :country_of_origin}
   it { should validate_length_of(:prod_name).is_at_most(100) }
   it("titleizes the name of a product") do
     product = Product.create({prod_name: "giant steps", cost: 3, country_of_origin: "USA" })

f0d20ed1746077e8d5d0d930db5f44529c09b9b7
cidsinga <56556816+cidsinga@users.noreply.github.com>
2020-01-24 10:19:13 -0800

    add aspects of validation and sessions

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,31 @@
+class UsersController < ApplicationController
+before_action :authorize, only: [:index, :show]
+before_action :set_user, only: [:show, :edit, :update, :destroy]
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      session[:user_id] = @user.id
+      binding.pry
+      if current_user.admin === true
+        flash[:notice] = "Welcome, Admin"
+      else
+        flash[:notice] = "You've successfully signed up!"
+      end
+      redirect_to "/products"
+    else
+      flash[:alert] = "There was a problem signing up."
+      redirect_to '/signup'
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:email, :password, :password_confirmation)
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_01_17_182137) do
+ActiveRecord::Schema.define(version: 2020_01_24_174003) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -26,6 +26,14 @@ ActiveRecord::Schema.define(version: 2020_01_17_182137) do
     t.string "content_body"
     t.integer "rating"
     t.integer "product_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "email"
+    t.string "password_hash"
+    t.string "password_salt"
+    t.string "user_name"
+    t.boolean "admin", default: false
   end
 
   add_foreign_key "reviews", "products"

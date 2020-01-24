19ed5b9f39fb127d674d3c2fc230e170006922b1
cidsinga <56556816+cidsinga@users.noreply.github.com>
2020-01-24 09:02:35 -0800

    add additions to rspec first test

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -2,10 +2,10 @@ require 'rails_helper'
 
 describe Product do
   it { should have_many (:reviews) }
-  it { should validate_presence_of :prod_name }
+  it { should validate_presence_of :prod_name, :cost, :country_of_origin }
   it { should validate_length_of(:prod_name).is_at_most(100) }
   it("titleizes the name of a product") do
-    product = Product.create({prod_name: "giant steps"})
+    product = Product.create({prod_name: "giant steps", cost: 3, country_of_origin: "USA" })
     expect(product.prod_name()).to(eq("Giant Steps"))
   end
 end
